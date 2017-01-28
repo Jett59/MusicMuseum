23dafc5f123a58772063dd6c971b08288e157bae
Jett <Jett@192.168.0.18>
2017-01-28 15:04:24 +1100

    Add music playing code and the JFugue library.

---
+++ src/main/java/Project.java
@@ -0,0 +1,10 @@
+import org.jfugue.player.Player;
+
+public class Project {
+
+	public static void main(String[] args) {
+		Player player = new Player();
+		player.play("b e6 g6 f6 e6h b6 a6w e6h f6h e6h g6 d6 f6 bh"); 
+	}
+
+}
